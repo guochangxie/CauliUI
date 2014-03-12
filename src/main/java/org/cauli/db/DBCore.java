e080b138da09f9897e619c4f0a22fefad9e6850e
王天庆 <tianqing.wang@qunar.com>
2014-03-12 15:15:46 +0800

    add two class

---
+++ src/main/java/org/cauli/db/DBCore.java
@@ -0,0 +1,7 @@
+package org.cauli.db;
+
+/**
+ * Created by tianqing.wang on 14-3-12
+ */
+public class DBCore {
+}
---
+++ src/main/java/org/cauli/verify/JSONAssert.java
@@ -0,0 +1,7 @@
+package org.cauli.verify;
+
+/**
+ * Created by tianqing.wang on 14-3-12
+ */
+public class JSONAssert {
+}
